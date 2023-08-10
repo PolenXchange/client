057c84cb67fe8ec8ad218106f7bb62e95d4c051e
igormuba <igorm-uba@hotmail.com>
2023-08-10 17:17:11 -0300

    swap functionality added

--- src/pages/SwapPage.js
+++ src/pages/SwapPage.js
@@ -1,13 +1,22 @@
 import React, { useState, useEffect } from "react";
-import { fetchOrderBook } from "../redux/store";
+import {
+  fetchOrderBook,
+  openLoginModal,
+  fetchAccount,
+  fetchMyOpenOrders,
+} from "../redux/store";
 import { useDispatch, useSelector } from "react-redux";
 import { debounce } from "debounce";
+import MyOrders from "../components/ExchangePage/MyOrders";
+
 const SwapPage = () => {
-  const [fromAmount, setFromAmount] = useState("");
-  const [toAmount, setToAmount] = useState("");
+  const [fromAmount, setFromAmount] = useState("0");
+  const [toAmount, setToAmount] = useState("0");
   const [conversionRate, setConversionRate] = useState(0.0);
   const [networkFee, setNetworkFee] = useState(0.0);
   const [totalCost, setTotalCost] = useState(0.0);
+  const [maxOffset, setMaxOffset] = useState(0.0);
+
   const [isHiveToHbd, setIsHiveToHbd] = useState(true);
   const [fromGreaterThanBalance, setFromGreaterThanBalance] = useState(true);
   const [priceImpact, setPriceImpact] = useState("0");
@@ -21,6 +30,7 @@ const SwapPage = () => {
   useEffect(() => {
     // Fetch order book data through Redux
     dispatch(fetchOrderBook());
+    dispatch(fetchAccount(username));
   }, [dispatch]);
 
   const handleFromAmountChange = (e) => {
@@ -113,10 +123,89 @@ const SwapPage = () => {
     setToAmount(fromAmount);
   };
 
+  const buyHIVE = async () => {
+    if (username && fromAmount > 0 && toAmount > 0) {
+      let expiration = new Date(Date.now());
+      expiration.setDate(expiration.getDate() + 27);
+      expiration = expiration.toISOString().split(".")[0];
+      const op = [
+        "limit_order_create",
+        {
+          orderid: Math.floor(Date.now() / 1000),
+          owner: account.name,
+          amount_to_sell: `${fromAmount.toFixed(3)} HBD`,
+          min_to_receive: `${(toAmount * (1 - maxOffset / 100)).toFixed(
+            3
+          )} HIVE`,
+          fill_or_kill: false,
+          expiration: expiration,
+        },
+      ];
+
+      await window.hive_keychain.requestBroadcast(
+        account.name,
+        [op],
+        "Active",
+        async (response) => {
+          console.log(response);
+          console.log("Operation sent");
+          dispatch(fetchAccount(username));
+          dispatch(fetchMyOpenOrders(username));
+          dispatch(fetchOrderBook());
+        }
+      );
+    } else if (!account.name) {
+      //not logged in
+      dispatch(openLoginModal());
+    }
+  };
+
+  const buyHBD = async () => {
+    console.log("buying hive");
+    console.log("account");
+    console.log(account);
+    console.log("account.name");
+    console.log(account.name);
+    if (account.name && fromAmount > 0 && toAmount > 0) {
+      let expiration = new Date(Date.now());
+      expiration.setDate(expiration.getDate() + 27);
+      expiration = expiration.toISOString().split(".")[0];
+      const op = [
+        "limit_order_create",
+        {
+          orderid: Math.floor(Date.now() / 1000),
+          owner: account.name,
+          amount_to_sell: `${fromAmount.toFixed(3)} HIVE`,
+          min_to_receive: `${(toAmount * (1 - maxOffset / 100)).toFixed(
+            3
+          )} HBD`,
+          fill_or_kill: false,
+          expiration: expiration,
+        },
+      ];
+
+      await window.hive_keychain.requestBroadcast(
+        account.name,
+        [op],
+        "Active",
+        async (response) => {
+          console.log(response);
+          console.log("Operation sent");
+          dispatch(fetchAccount(username));
+          dispatch(fetchMyOpenOrders(username));
+          dispatch(fetchOrderBook());
+        }
+      );
+    } else if (!account.name) {
+      //not logged in
+      dispatch(openLoginModal());
+    }
+  };
+
   // Add more event handlers and logic as needed
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
       <div className="w-full max-w-md p-6 bg-white rounded-lg shadow-md">
         <h2 className="text-2xl font-semibold mb-4">Token Swap</h2>
 
@@ -157,7 +246,14 @@ const SwapPage = () => {
           <button
             className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none focus:ring"
             onClick={() => {
-              // Implement swap functionality here
+              console.log("isHiveToHbd");
+              console.log(isHiveToHbd);
+
+              if (!isHiveToHbd) {
+                buyHIVE();
+              } else if (isHiveToHbd) {
+                buyHBD();
+              }
             }}
           >
             Swap Now
@@ -169,6 +265,9 @@ const SwapPage = () => {
             Flip
           </button>
         </div>
+      </div>
+      <div className="w-full max-w-md p-6 bg-white rounded-lg shadow-md">
+        <MyOrders />
       </div>
     </div>
   );
