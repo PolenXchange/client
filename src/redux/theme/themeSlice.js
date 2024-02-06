a9725aef1c64e9713324cbd00f5942532d5005c4
igormuba <igorm-uba@hotmail.com>
2024-02-06 11:58:42 -0300

    dark theme persistance

--- src/redux/theme/themeSlice.js
+++ src/redux/theme/themeSlice.js
@@ -1,13 +1,16 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const localStorageTheme = localStorage.getItem("theme");
+
 const themeSlice = createSlice({
   name: "theme",
   initialState: {
-    currentTheme: "dark", // Default theme is light
+    currentTheme: localStorageTheme ? localStorageTheme : "dark", // Default theme is dark if not set in local storage
   },
   reducers: {
     setTheme: (state, action) => {
       state.currentTheme = action.payload;
+      localStorage.setItem("theme", action.payload); // Save the theme preference to local storage
     },
   },
 });
